b85931d11e55ae0cb696b10d589dd75d290ee35c
imanezahri002 <imanezh5@gmail.com>
2024-11-11 13:34:19 +0100

    deuxeme commit

--- breif5.js
+++ breif5.js
@@ -8,9 +8,9 @@ function formaa(){
   }
     Conteure++;
   
-    let ConteureStep = document.getElementById('step-'+(Conteure+1));
+    let ConteureStep = document.getElementById('step-'+(Conteure));
     ConteureStep.click();
-    tab[Conteure].style.zIndex='100';
+    tab[Conteure].style.zIndex='1';
 
 }
 
@@ -23,8 +23,44 @@ let EnfantCounteur = 0;
 const adultPrice = 500;
 const EnfantPrice = 100;
 
+let Adultplace = document.getElementById('Adultplace');
+let Enfantplace = document.getElementById('Enfantplace');
+const prixTotale = document.getElementById("PrixTotal");
 
+function prixtot(){
+  let total = (adultCounteur * adultPrice) + (EnfantCounteur * EnfantPrice);
+  prixTotale.innerHTML = `prix totale ${total} DH`;
+}
 
+function increment() {
+  maxCount = adultCounteur + EnfantCounteur;
+  if(maxCount<8 ){
+    adultCounteur += 1;
+    Adultplace.innerHTML = adultCounteur;
+  }
+  prixtot();
+}
+function decrement() {
+  if(adultCounteur>0){
+  adultCounteur -= 1;
+  Adultplace.innerHTML = adultCounteur;
+}
+prixtot()
+
+}
+
+function incremente() {
+  EnfantCounteur += 1;
+  Enfantplace.innerHTML = EnfantCounteur;
+  prixtot()
+}
+function decrementee() {
+  if(EnfantCounteur>0){
+    EnfantCounteur-= 1;
+    Enfantplace.innerHTML = EnfantCounteur;
+}
+prixtot()
+}
 
 
 
